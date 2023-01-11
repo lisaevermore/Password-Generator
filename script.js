26ca6eac79812fe03f97a486c6e46fb3f7bfda78
lisaevermore <70776430+lisaevermore@users.noreply.github.com>
2023-01-11 23:09:03 +0000

    fixing password function

--- script.js
+++ script.js
@@ -100,23 +100,62 @@ var upperCasedCharacters = [
 // Once prompts are answered then the password should be generated and displayed in an alert or written to the page
 
 // Function to prompt user for password options
+let user = 0;
+let userPreference = [];
 function getPasswordOptions() {
+    user = prompt("Password Length?");
+  while (user <= 10 || user >= 64){
+    alert("Your Password Length need to be more than 10 and less than 64");
+    return getPasswordOptions(); 
+  }
+
+  let getSpecialchar = confirm("Any special characters?");
+  let getInter = confirm("Want to add any number?");
+  let getLowerCase = confirm("Any lowercase characters?");
+  let getUpperCase = confirm("Any uppercase characters?");
+  while (!getSpecialchar && !getInter && !getLowerCase && !getUpperCase ){ // ensure user chooses at least one option
+    alert("You need to pick atleast one types of Character");
+     getSpecialchar = confirm("Any special characters?");
+     getInter = confirm("Want to add any number?");
+     getLowerCase = confirm("Any lowercase characters?");
+     getUpperCase = confirm("Any uppercase characters?");
+    return;
+  };
+
+   userPreference = []; // arr list of the user perference for the password 
+  if(getSpecialchar){
+    userPreference = userPreference.concat(specialCharacters);
+  }
+  if(getInter){
+    userPreference = userPreference.concat(numericCharacters);
+  }
+  if(getLowerCase){
+    userPreference = userPreference.concat(lowerCasedCharacters);
+  }
+  if(getUpperCase){
+    userPreference = userPreference.concat(upperCasedCharacters);
+  }
+  return (user);
  
-
 }
-
+//console.log(user);
+//console.log(getPasswordOptions())
+// i need user input password length with the user perference to make a random password
 // Function for getting a random element from an array
 function getRandom(arr) {
-
+  var randomElement = random.choice(getPasswordOptions())
+  return randomElement;
 }
+//console.log(getRandom())
 
 // Function to generate password with user input
 function generatePassword() {
   let randomPassword = "";
-  for(let i = 0; i < getPasswordOptions(); i++){
+  var lengthArr = getPasswordOptions()
+  for(let i = 0; i < lengthArr; i++){
     randomPassword += userPreference[Math.floor(Math.random() * userPreference.length)];
-  } return generatePassword;
-
+  } 
+  return randomPassword;
 }
 
 // Get references to the #generate element
